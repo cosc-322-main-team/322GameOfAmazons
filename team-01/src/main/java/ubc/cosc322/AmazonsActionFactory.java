563c2ab9cc6b1958113c7452e8551d9278a06407
Rylan Cox <rylanrcox@gmail.com>
2021-03-01 21:14:52 -0800

    Filled out methods of ActionFactory

--- team-01/src/main/java/ubc/cosc322/AmazonsActionFactory.java
+++ team-01/src/main/java/ubc/cosc322/AmazonsActionFactory.java
@@ -6,37 +6,66 @@ import java.util.ArrayList;
 public class AmazonsActionFactory {
   public ArrayList<AmazonsAction> getActions(ArrayList<Integer> state) {
     ArrayList<AmazonsAction> list = new ArrayList();
-    //Find queen Positions
-    for(int i = 0; i <= 9; i++) {
-      for (int j = 0; j <= 9; i++) {
-
-      }
+    ArrayList<Integer> queenCurrents= getQueenCurrents();
+    while(!queenCurrents.isEmpty()) {
+      ArrayList<Integer> queenTargets = getQueenTargets(i, j);
+      ArrayList<Integer> arrowTarget = getArrowTarget(queenTarget);
+      //Add these to the Amazons action list.
+      AmazonsAction newAction = new AmazonsAction(queenCurrent, queenTarget, arrowTarget);
+      list.add(newAction);
     }
-    //Turn these into different methods.
     return list;
   }
-  private boolean withinMoves(int x, int y){
+  private boolean withinMoves(int queenX, int queenY, int x, int y){
     //1 Calculate vertical and horizontal moves
     //2 Calculate diagonal moves
     return false;
   }
-  private AmazonsAction getQueenMove() {
-    //Possible queen moves, is gonna call get arrow
-    for(int i = 0; i <= 9; i++){
-      for(int j = 0; j <= 9; i++){
-        if(state.postion(i, j) == 0 && withinMoves(queenPosition.x, queenPosition.y, i,j)) {
+  private ArrayList<Integer> getQueenCurrents(){
+    ArrayList<Integer> queenCurrents = new ArrayList();
+    for(int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; i++) {
+        if(state.position(i,j) == 2){
+          //Create an arraylist for the queens current position
+          queenCurrents.add(i);
+          queenCurrents.add(j);
         }
       }
     }
+    return queenCurrents;
   }
-  private AmazonsAction getArroveMove(){
-    //Returns where arrows can move
-    for(int x = 0; x <= 9; x++){
-      for(int y = 0; y <= 9; y++){
-        if(state.postion(x, y) == 0 && withinMoves(i,j,x,y)) {
-
+  private ArrayList<Integer>getQueenTargets(int queenPositionx, int queenPositiony) {
+    //Possible queen moves, is gonna call get arrow
+    ArrayList<Integer> targets = new ArrayList();
+    for(int i = 0; i < 10; i++){
+      for(int j = 0; j < 10; i++){
+        if(state.position(i, j) == 0 && withinMoves(queenPositionx, queenPositiony, i,j)) {
+          targets.add(i);
+          targets.add(j);
         }
       }
     }
+    return guySmells;
+  }
+  private ArrayList<Integer> getArrowTarget(ArrayList<Integer> queenTargets) {
+    //Returns where arrows can move
+    ArrayList<Integer> arrowTargets = new ArrayList();
+    //A while loop going through the list of queen targets.
+    while (!queenTargets.isEmpty()) {
+      //Emptying out the arraylist getting our x and y values of the queen targets in the rotating order.
+      int i = queenTargets.remove(0);
+      int j = queenTargets.remove(0);
+      //A double for loop covering each possible area an arrow can be shot from each possible queen target.
+      for (int x = 0; x < 10; x++) {
+        for (int y = 0; y < 10; y++) {
+          if (state.position(x, y) == 0 && withinMoves(i, j, x, y)) {
+            //Adding rotating x and ys to our arraylist of possible arrow targets
+            arrowTargets.add(x);
+            arrowTargets.add(y);
+          }
+        }
+      }
+    }
+    return arrowTargets;
   }
 }
