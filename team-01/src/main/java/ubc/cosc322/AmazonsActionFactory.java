fa2db228397e6dfa6d4855336d5ee2ce7453792b
Rylan Cox <rylanrcox@gmail.com>
2021-03-01 23:02:21 -0800

    fail to debug out of bounds error

--- team-01/src/main/java/ubc/cosc322/AmazonsActionFactory.java
+++ team-01/src/main/java/ubc/cosc322/AmazonsActionFactory.java
@@ -1,24 +1,25 @@
 package ubc.cosc322;
 
 
+import java.util.Arrays;
 import java.util.ArrayList;
 
 public class AmazonsActionFactory {
-  public ArrayList<AmazonsAction> getActions(ArrayList<Integer> state) {
+  public ArrayList<AmazonsAction> getActions(AmazonsLocalBoard board) {
     ArrayList<AmazonsAction> list = new ArrayList();
-    ArrayList<ArrayList<Integer>> allQueens= getAllQueenCurrents();
+    ArrayList<ArrayList<Integer>> allQueens= getAllQueenCurrents(board);
 
     while(!allQueens.isEmpty()) {
 
       ArrayList<Integer> queenCurrent= allQueens.remove(0);
 
-      ArrayList<ArrayList<Integer>> allQueenTargets = getTargets(queenCurrent.get(0), queenCurrent.get(1));
+      ArrayList<ArrayList<Integer>> allQueenTargets = getTargets(queenCurrent.get(0), queenCurrent.get(1), board);
 
       while(!allQueenTargets.isEmpty()) {
 
         ArrayList<Integer> queenTarget = allQueenTargets.remove(0);
 
-        ArrayList<ArrayList<Integer>> allArrowTargets = getTargets(queenTarget.get(0), queenTarget.get(1));
+        ArrayList<ArrayList<Integer>> allArrowTargets = getTargets(queenTarget.get(0), queenTarget.get(1), board);
 
         while(!allArrowTargets.isEmpty()){
 
@@ -35,34 +36,28 @@ public class AmazonsActionFactory {
   private boolean withinMoves(int queenX, int queenY, int x, int y){
     //1 Calculate vertical and horizontal moves
     //2 Calculate diagonal moves
-    return false;
+    return true;
   }
-  private ArrayList<ArrayList<Integer>> getAllQueenCurrents(){
+  private ArrayList<ArrayList<Integer>> getAllQueenCurrents(AmazonsLocalBoard board){
     ArrayList<ArrayList<Integer>>  queenCurrents = new ArrayList();
     for(int i = 0; i < 10; i++) {
-      for (int j = 0; j < 10; i++) {
-        if(state.position(i,j) == 2){
-          //Create an arraylist for the queens current position
-          ArrayList<Integer> queenPosition = new ArrayList();
-          queenPosition.add(i);
-          queenPosition.add(j);
-          queenCurrents.add(queenPosition);
-
+      for (int j = 0; j < 10; j++) {
+        ArrayList<Integer> position = new ArrayList(Arrays.asList(i,j));
+        if(board.getPositionValue(position) == 2){
+          queenCurrents.add(position);
         }
       }
     }
     return queenCurrents;
   }
-  private ArrayList<ArrayList<Integer>> getTargets(int startPositionX, int startPositionY) {
+  private ArrayList<ArrayList<Integer>> getTargets(int x, int y, AmazonsLocalBoard board) {
     //Possible queen moves, is gonna call get arrow
     ArrayList<ArrayList<Integer>> targets = new ArrayList();
-    for(int targetPositionX = 0; targetPositionX < 10; targetPositionX++){
-      for(int targetPositionY = 0; targetPositionY < 10; targetPositionY++){
-        if(state.position(targetPositionX, targetPositionY) == 0 && withinMoves(startPositionX, startPositionY, targetPositionX,targetPositionY)) {
-          ArrayList<Integer> targetPosition = new ArrayList();
-          targetPosition.add(targetPositionX);
-          targetPosition.add(targetPositionY);
-          targets.add(targetPosition);
+    for(int i = 0; i < 10; i++){
+      for(int j = 0; j < 10; j++){
+        ArrayList<Integer> position = new ArrayList(Arrays.asList(i,j));
+        if(board.getPositionValue(position) == 0 && withinMoves(x, y, i,j)) {
+          targets.add(position);
         }
       }
     }
