05fa16ae01ea6b5dc70b3f2d28d3e9388730a29e
Rylan Cox <rylanrcox@gmail.com>
2021-03-01 21:40:03 -0800

    Finish all methods except withinMoves

--- team-01/src/main/java/ubc/cosc322/AmazonsActionFactory.java
+++ team-01/src/main/java/ubc/cosc322/AmazonsActionFactory.java
@@ -6,13 +6,39 @@ import java.util.ArrayList;
 public class AmazonsActionFactory {
   public ArrayList<AmazonsAction> getActions(ArrayList<Integer> state) {
     ArrayList<AmazonsAction> list = new ArrayList();
-    ArrayList<Integer> queenCurrents= getQueenCurrents();
-    while(!queenCurrents.isEmpty()) {
-      ArrayList<Integer> queenTargets = getQueenTargets(i, j);
-      ArrayList<Integer> arrowTarget = getArrowTarget(queenTarget);
-      //Add these to the Amazons action list.
-      AmazonsAction newAction = new AmazonsAction(queenCurrent, queenTarget, arrowTarget);
-      list.add(newAction);
+    ArrayList<Integer> allQueens= getAllQueenCurrents();
+
+    while(!allQueens.isEmpty()) {
+      int queenX = allQueens.remove(0);
+      int queenY = allQueens.remove(0);
+
+      ArrayList<Integer> queenCurrent = new ArrayList();
+      queenCurrent.add(queenX);
+      queenCurrent.add(queenY);
+
+      ArrayList<Integer> allQueenTargets = getTargets(queenX, queenY);
+      while(!allQueenTargets.isEmpty()) {
+        int queenTargetX = allQueenTargets.remove(0);
+        int queenTargetY = allQueenTargets.remove(0);
+
+        ArrayList<Integer> queenTarget = new ArrayList();
+        queenTarget.add(queenTargetX);
+        queenTarget.add(queenTargetY);
+
+        ArrayList<Integer> allArrowTargets = getTargets(queenTargetX, queenTargetY);
+        while(!allArrowTargets.isEmpty()){
+          int arrowTargetX = allArrowTargets.remove(0);
+          int arrowTargetY = allArrowTargets.remove(0);
+
+          ArrayList<Integer> arrowTarget = new ArrayList();
+          arrowTarget.add(arrowTargetX);
+          arrowTarget.add(arrowTargetY);
+
+          AmazonsAction newAction = new AmazonsAction(queenCurrent, queenTarget, arrowTarget);
+          list.add(newAction);
+        }
+      }
+      //Add these to the Amazons action lists
     }
     return list;
   }
@@ -21,7 +47,7 @@ public class AmazonsActionFactory {
     //2 Calculate diagonal moves
     return false;
   }
-  private ArrayList<Integer> getQueenCurrents(){
+  private ArrayList<Integer> getAllQueenCurrents(){
     ArrayList<Integer> queenCurrents = new ArrayList();
     for(int i = 0; i < 10; i++) {
       for (int j = 0; j < 10; i++) {
@@ -34,38 +60,17 @@ public class AmazonsActionFactory {
     }
     return queenCurrents;
   }
-  private ArrayList<Integer>getQueenTargets(int queenPositionx, int queenPositiony) {
+  private ArrayList<Integer> getTargets(int startPositionX, int startPositionY) {
     //Possible queen moves, is gonna call get arrow
     ArrayList<Integer> targets = new ArrayList();
-    for(int i = 0; i < 10; i++){
-      for(int j = 0; j < 10; i++){
-        if(state.position(i, j) == 0 && withinMoves(queenPositionx, queenPositiony, i,j)) {
-          targets.add(i);
-          targets.add(j);
+    for(int targetPositionX = 0; targetPositionX < 10; targetPositionX++){
+      for(int targetPositionY = 0; targetPositionY < 10; targetPositionY++){
+        if(state.position(targetPositionX, targetPositionY) == 0 && withinMoves(startPositionX, startPositionY, targetPositionX,targetPositionY)) {
+          targets.add(targetPositionX);
+          targets.add(targetPositionY);
         }
       }
     }
-    return guySmells;
-  }
-  private ArrayList<Integer> getArrowTarget(ArrayList<Integer> queenTargets) {
-    //Returns where arrows can move
-    ArrayList<Integer> arrowTargets = new ArrayList();
-    //A while loop going through the list of queen targets.
-    while (!queenTargets.isEmpty()) {
-      //Emptying out the arraylist getting our x and y values of the queen targets in the rotating order.
-      int i = queenTargets.remove(0);
-      int j = queenTargets.remove(0);
-      //A double for loop covering each possible area an arrow can be shot from each possible queen target.
-      for (int x = 0; x < 10; x++) {
-        for (int y = 0; y < 10; y++) {
-          if (state.position(x, y) == 0 && withinMoves(i, j, x, y)) {
-            //Adding rotating x and ys to our arraylist of possible arrow targets
-            arrowTargets.add(x);
-            arrowTargets.add(y);
-          }
-        }
-      }
-    }
-    return arrowTargets;
+    return targets;
   }
 }
