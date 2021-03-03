c05a8973e838c2d0bd50ae750311bff53e3c5350
Rylan Cox <rylanrcox@gmail.com>
2021-03-03 14:58:54 -0800

    Fix more spacing errors

--- team-01/src/main/java/ubc/cosc322/AmazonsActionFactory.java
+++ team-01/src/main/java/ubc/cosc322/AmazonsActionFactory.java
@@ -13,16 +13,21 @@ public class AmazonsActionFactory {
     while(!allQueens.isEmpty()) {
       //Iterating through the allQueens arrayList removing them one by one.
       ArrayList<Integer> queenCurrent= allQueens.remove(0);
+
       //Creating a double array list of all possible moves for the current queen.
       ArrayList<ArrayList<Integer>> allQueenTargets = getTargets(queenCurrent.get(0), queenCurrent.get(1), board);
+
       while(!allQueenTargets.isEmpty()) {
         //Iterating through all the possible moves and removing them one by one
         ArrayList<Integer> queenTarget = allQueenTargets.remove(0);
+
         //Creating a double array list of all possible arrow moves for the current queen target position
         ArrayList<ArrayList<Integer>> allArrowTargets = getTargets(queenTarget.get(0), queenTarget.get(1), board);
+
         while(!allArrowTargets.isEmpty()){
           //Iterating through all the arrow target locations and removing them.
           ArrayList<Integer> arrowTarget = allArrowTargets.remove(0);
+
           //Adding the current arrow target, the queen target position, and the current queen to the list of AmazonsActions
           AmazonsAction newAction = new AmazonsAction(queenCurrent, queenTarget, arrowTarget);
           list.add(newAction);
@@ -58,7 +63,7 @@ public class AmazonsActionFactory {
       for(int j = 0; j < 10; j++){
         ArrayList<Integer> position = new ArrayList(Arrays.asList(i,j));
         //Checking if the target position is open and if it is within moves.
-        if(board.getPositionValue(position) == 0 && withinMoves(x, y, i,j)) {
+        if(board.getPositionValue(position) == 0 && withinMoves(x, y, i, j)) {
           targets.add(position);
         }
       }
